08d1d3b0a3d407392d3d636be64b66aa2777f0ae
Celaldogan Gunes <celaldogan2002@outlook.com>
2025-06-26 16:16:20 +0300

    A public page to view a list of all upcoming events.

---
+++ Pages/Events/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace UniBazaarLite.Pages.Events // ← BU SATIR ÖNEMLİ
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IRepository<Event> _eventRepo;
+        private readonly IConfiguration _config;
+
+        public List<Event> Events { get; set; } = new();
+        public string? SiteTitle { get; set; }
+
+        public IndexModel(IRepository<Event> eventRepo, IConfiguration config)
+        {
+            _eventRepo = eventRepo;
+            _config = config;
+        }
+
+        public void OnGet()
+        {
+            Events = _eventRepo.GetAll().ToList();
+            SiteTitle = _config["SiteSettings:SiteTitle"];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,32 @@ builder.Services.AddSingleton(typeof(IRepository<ClassifiedItem>), typeof(InMemo
 
 var app = builder.Build();
 
+
+var eventRepo = app.Services.GetRequiredService<IRepository<Event>>();
+eventRepo.Add(new Event { Title = "Hackathon", Description = "Coding contest 2!", Date = DateTime.Today.AddDays(3) });
+eventRepo.Add(new Event { Title = "Career Fair", Description = "Meet companies.", Date = DateTime.Today.AddDays(7) });
+
+/*
+using (var scope = app.Services.CreateScope())
+{
+    var repo = scope.ServiceProvider.GetRequiredService<IRepository<Event>>();
+
+    repo.Add(new Event
+    {
+        Title = "Hackathon",
+        Description = "A 24-hour coding marathon!",
+        Date = DateTime.Today.AddDays(2)
+    });
+
+    repo.Add(new Event
+    {
+        Title = "Job Fair",
+        Description = "Meet tech companies on campus.",
+        Date = DateTime.Today.AddDays(5)
+    });
+}*/
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -25,3 +51,4 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
