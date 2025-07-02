3f10855facb2aa7237928f15c6470a98a34c198b
Celaldogan Gunes <celaldogan2002@outlook.com>
2025-07-02 11:45:38 +0300

    Update IRepository.cs

--- Data/IRepository.cs
+++ Data/IRepository.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
 
+// Yapımcı: Hüseyin Kaplan
+// Bu interface, repository tasarım deseninin temel sözleşmesini belirler.
+// Generic olarak çalışır ve T tipindeki nesneler üzerinde CRUD işlemleri sağlar.
+
 public interface IRepository<T>
 {
-    IEnumerable<T> GetAll();
-    T GetById(int id);
-    void Add(T item);
-    void Update(T item);
-    void Delete(int id);
+    IEnumerable<T> GetAll(); // Tüm kayıtları getir
+    T GetById(int id);       // Belirli Id'ye sahip kaydı getir
+    void Add(T item);        // Yeni kayıt ekle
+    void Update(T item);     // Var olan kaydı güncelle
+    void Delete(int id);     // Id ile kaydı sil
 }
