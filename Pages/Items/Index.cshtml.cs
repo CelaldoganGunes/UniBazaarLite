9999cee02a51f953bbf689b5cb83ab51996b458d
Celaldogan Gunes <celaldogan2002@outlook.com>
2025-07-02 11:57:43 +0300

    Update Index.cshtml.cs

--- Pages/Items/Index.cshtml.cs
+++ Pages/Items/Index.cshtml.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+// Yapımcı: Hüseyin Kaplan
+// Subsystem B - Razor Pages kısmı
+// Bu sayfa, tüm ilanları listeleyen Index Razor Page’in backend kodudur.
+
 namespace UniBazaarLite.Pages.Items
 {
     public class IndexModel : PageModel
     {
-        private readonly IRepository<ClassifiedItem> _itemRepo;
+        private readonly IRepository<ClassifiedItem> _itemRepo; 
+        // Bellek içi repository ile ilanları çeker
 
         public List<ClassifiedItem> Items { get; set; } = new();
+        // View’da kullanılacak ilan listesi
 
         public IndexModel(IRepository<ClassifiedItem> itemRepo)
         {
@@ -15,6 +21,7 @@ namespace UniBazaarLite.Pages.Items
 
         public void OnGet()
         {
+            // Sayfa GET isteği aldığında tüm ilanları getir
             Items = _itemRepo.GetAll().ToList();
         }
     }
