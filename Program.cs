fb6a4e9f3fc8dd3b3d361d1a44e528ff265a0e76
Celaldogan Gunes <celaldogan2002@outlook.com>
2025-07-02 11:59:31 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Yapımcı: Üçümüz (Celaldoğan Güneş, Burak Kılıç, Hüseyin Kaplan)
+// Bu dosya uygulamanın giriş noktası. Dependency Injection container ayarları,
+// örnek veri ekleme ve pipeline konfigürasyonları burada yapılır.
+
+// DI container'a servisleri ekliyoruz
 builder.Services.AddSingleton(typeof(IRepository<Event>), typeof(InMemoryRepository<Event>));
 builder.Services.AddSingleton(typeof(IRepository<ClassifiedItem>), typeof(InMemoryRepository<ClassifiedItem>));
 builder.Services.AddSingleton<IUserContextService, FakeUserContextService>();
@@ -8,22 +12,20 @@ builder.Services.AddScoped(typeof(ValidateEntityExistsFilter<>));
 
 builder.Services.AddControllersWithViews(options =>
 {
-    options.Filters.Add<LogActivityFilter>();
+    options.Filters.Add<LogActivityFilter>(); // MVC için global log filter
 });
 
 builder.Services.AddRazorPages(options =>
 {
-    options.Conventions.ConfigureFilter(new LogActivityFilter());
+    options.Conventions.ConfigureFilter(new LogActivityFilter()); // Razor Pages için global log filter
 });
-
 
 var app = builder.Build();
 
-
+// Örnek verileri uygulama başladığında belleğe ekliyoruz
 var eventRepo = app.Services.GetRequiredService<IRepository<Event>>();
 eventRepo.Add(new Event { Title = "Hackathon", Description = "Coding contest 2!", Date = DateTime.Today.AddDays(3) });
 eventRepo.Add(new Event { Title = "Career Fair", Description = "Meet companies.", Date = DateTime.Today.AddDays(7) });
-
 
 var itemRepo = app.Services.GetRequiredService<IRepository<ClassifiedItem>>();
 itemRepo.Add(new ClassifiedItem
@@ -41,15 +43,12 @@ itemRepo.Add(new ClassifiedItem
     Seller = "Zeynep"
 });
 
-
-// Configure the HTTP request pipeline.
+// HTTP pipeline ayarları
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -60,6 +59,7 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapControllers();
-app.UseStatusCodePagesWithRedirects("/Error");
-app.Run();
 
+app.UseStatusCodePagesWithRedirects("/Error"); // ör: 404 için /Error'a yönlendir
+
+app.Run();
