ea97f67ae48197fbb4d1985abc91f48b0f48af1c
Celaldogan Gunes <celaldogan2002@outlook.com>
2025-07-02 11:52:36 +0300

    Update Event.cs

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,14 +1,23 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
+// Yapımcı: Üçümüz (Celaldoğan Güneş, Burak Kılıç, Hüseyin Kaplan)
+// Bu sınıf, Subsystem A (Events System) için etkinlik modelimizi tanımlar.
+// DataAnnotations ile formda tarih alanını daha doğru göstermeyi sağladık.
+
 public class Event
 {
-    public int Id { get; set; }
+    public int Id { get; set; } 
+    // Benzersiz Id (InMemoryRepository tarafından otomatik atanır)
 
     public string Title { get; set; } = "";
+    // Etkinlik başlığı (zorunluluğu biz controller'da veya view'da kontrol ediyoruz)
 
     public string Description { get; set; } = "";
+    // Etkinlik açıklaması
 
     [DataType(DataType.Date)]
     public DateTime Date { get; set; }
+    // Bu DataAnnotation, ASP.NET Core'un formda HTML5 date picker çıkarmasını sağlar
+    // Aynı zamanda validasyon mesajlarında 'geçerli bir tarih girin' şeklinde uyarı döner
 }
